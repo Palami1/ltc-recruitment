2c11f0720c888f1be0c83f839bbc4d5722c3e3f4
Palami <palamiphomaly@gmail.com>
2026-07-13 23:53:25 +0700

    Fix: revert vite charset option that caused TS build error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    charset: 'utf8',
-  },
-  server: {
-    headers: {
-      'Content-Type': 'text/html; charset=utf-8',
-    },
-  },
 })
+
