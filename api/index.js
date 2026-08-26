10f5b2d66096166e7c37da88410d1c8da8133f1d
Palami <palamiphomaly@gmail.com>
2026-08-26 20:19:42 +0700

    Fix Vercel function includeFiles and error handler in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,13 @@
-const app = require('../server/index.js');
+let app;
+try {
+  app = require('../server/index.js');
+} catch (err) {
+  console.error('Failed to load server/index.js:', err);
+  const express = require('express');
+  app = express();
+  app.all('*', (req, res) => {
+    res.status(500).json({ error: 'Server initialization error', details: err.message, stack: err.stack });
+  });
+}
 
 module.exports = app;
