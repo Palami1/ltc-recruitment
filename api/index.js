b5f20e17f51b6fb7761bb3e3cc4b1f7517bf5192
Palami <palamiphomaly@gmail.com>
2026-08-26 20:24:47 +0700

    Update Vercel function handler in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,5 @@
-let app;
-try {
-  app = require('../server/index.js');
-} catch (err) {
-  console.error('Failed to load server/index.js:', err);
-  const express = require('express');
-  app = express();
-  app.all('*', (req, res) => {
-    res.status(500).json({ error: 'Server initialization error', details: err.message, stack: err.stack });
-  });
-}
+const app = require('../server/index.js');
 
-module.exports = app;
+module.exports = (req, res) => {
+  return app(req, res);
+};
